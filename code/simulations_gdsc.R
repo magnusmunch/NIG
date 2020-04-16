fe54c9f1cbdb9c39e276406d137eb5a6bb988798
M.M. Münch <munchmm@aldebaran.backup.leidenuniv.nl>
2020-04-16 08:04:47 +0200

    reran simulations

--- code/simulations_gdsc.R
+++ code/simulations_gdsc.R
@@ -210,7 +210,7 @@ x <- scale(expr$expr[, order(-apply(expr$expr, 2, sd))[1:p]])
 nfolds <- 10
 methods <- c("NIG-", "NIG", "ridge", "lasso", "xtune")
 control.semnig <- list(conv.post=TRUE, trace=FALSE, epsilon.eb=1e-5, 
-                       epsilon.vb=1e-3, maxit.eb=2000, maxit.vb=1, 
+                       epsilon.vb=1e-3, maxit.eb=500, maxit.vb=1, 
                        maxit.post=100, maxit.block=0)
 control.ebridge <-list(epsilon=sqrt(.Machine$double.eps), maxit=500, 
                        trace=FALSE, glmnet.fit2=FALSE, beta2=FALSE)
@@ -374,7 +374,7 @@ x <- scale(expr$expr[, order(-apply(expr$expr, 2, sd))[1:p]])
 nfolds <- 10
 methods <- c("NIG-", "NIG", "ridge", "lasso", "xtune", "ebridge")
 control.semnig <- list(conv.post=TRUE, trace=FALSE, epsilon.eb=1e-5, 
-                       epsilon.vb=1e-3, maxit.eb=1, maxit.vb=1, 
+                       epsilon.vb=1e-3, maxit.eb=500, maxit.vb=1, 
                        maxit.post=100, maxit.block=0)
 control.ebridge <-list(epsilon=sqrt(.Machine$double.eps), maxit=500, 
                        trace=FALSE, glmnet.fit2=FALSE, beta2=FALSE)
@@ -555,7 +555,7 @@ x <- scale(expr$expr[, order(-apply(expr$expr, 2, sd))[1:p]])
 nfolds <- 10
 methods <- c("NIG-", "NIG", "ridge", "lasso", "xtune", "ebridge")
 control.semnig <- list(conv.post=TRUE, trace=FALSE, epsilon.eb=1e-5, 
-                       epsilon.vb=1e-3, maxit.eb=2000, maxit.vb=1, 
+                       epsilon.vb=1e-3, maxit.eb=500, maxit.vb=1, 
                        maxit.post=100, maxit.block=0)
 control.ebridge <-list(epsilon=sqrt(.Machine$double.eps), maxit=500, 
                        trace=FALSE, glmnet.fit2=FALSE, beta2=FALSE)
